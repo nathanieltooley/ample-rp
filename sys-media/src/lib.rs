23e6076dde9e985a2453daee48db7a95dd297cd9
Nathaniel Tooley <gotablast99@gmail.com>
2025-07-29 00:32:51 -0500

    docs: some plans for linux

--- sys-media/src/lib.rs
+++ sys-media/src/lib.rs
@@ -7,13 +7,16 @@ pub mod consts;
 
 #[derive(Debug, Clone)]
 pub struct MediaInfo {
+    /// Name of the app or executable that started playing this media 
     pub player_name: String,
     pub artist_name: String,
     pub song_name: String,
     pub album_name: String,
     pub status: MediaStatus,
     pub media_type: MediaType,
+    /// Length of media in microseconds
     pub end_time: i64,
+    /// Amount of time having watched / listened to media in microseconds
     pub current_position: i64,
 }
 
@@ -64,40 +67,29 @@ impl fmt::Display for MediaError {
 }
 
 /// Get the currently playing song's info including what app started playing it.
-/// Blocks on windows async calls.
+/// Blocks execution if waiting on async or syscalls.
 pub fn get_current_playing_info() -> Result<Option<MediaInfo>, MediaError> {
     if cfg!(windows) {
         let session = win_media::get_current_session()?;
         win_media::get_current_session_info(session).map_err(|err| err.into())
     } else {
+        // Possible ways I've found to get info on linux:
+        // - playerctl
+        // This could be done the "dirty" way by using processes and piping that info inside the library.
+        //
+        // The other option is using the playerctl "library" but this seems more complicated than just a libplayerctl sort of thing.
+        // It also looks like to use the playerctl "library," we'd have run Glib's EventLoop and listen for events? Which would require
+        // a more complicated API or possibly an explicit separation of functions. Basically, windows would have a function and linux would need an
+        // init or start function and then a normal function? Maybe have the function agnostic but init on windows is a no-op? 
+        // https://github.com/altdesktop/playerctl/tree/master
+        // For library route: https://gtk-rs.org/
+        //
+        // In either case this does introduce a dependency on playerctl which is outside of Rust. I'm not exactly sure how to depend explicitly
+        // on a system binary.
+        //
+        // - from scratch?
+        // If there is a nice way to "ask" the OS about info from the current media player, we might be able to sidestep any gtk / GLib stuff.
+        // However, I fear this is actually not simple to do.
         todo!()
     }
-}
-
-// pub fn get_all_sessions_info() -> Result<Vec<MediaInfo>, MediaError> {
-//     if cfg!(windows) {
-
-//     }
-//     let media_controller = GlobalSystemMediaTransportControlsSessionManager::RequestAsync()?;
-//     let media_controller = media_controller.get()?;
-
-//     let sessions = media_controller.GetSessions()?;
-
-//     let session_infos = sessions.into_iter().map(|session| {
-//         let player = session.SourceAppUserModelId()?;
-//         let media_props = session.TryGetMediaPropertiesAsync()?.get()?;
-
-//         MediaInfo {
-//             player_name: player.to_string_lossy(),
-//             artist_name: media_props.Artist()?.to_string_lossy(),
-//             song_name: media_props.Title()?.to_string_lossy(),
-//             album_name: media_props.AlbumTitle()?.to_string_lossy()
-//         }
-//     }).collect();
-
-//     Ok(session_infos)
-// }
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
 }
